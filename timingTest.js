6f191fd683ee316951a281453510e1b549985fd9
Alec Lownes <alec.lownes@gmail.com>
2016-06-17 10:44:38 -0400

    Add timing test

---
+++ timingTest.js
@@ -0,0 +1,16 @@
+var RollingStringHash = require('./index');
+var fs = require('fs');
+
+var csv = '';
+var multiplier = 1000000;
+var rh = new RollingStringHash();
+for (var x = 1; x < 21; x++){
+  var start = Date.now();
+  for (var y = 0; y < multiplier; y++){
+    rh.addRight('a');
+  }
+  var end = Date.now();
+  csv += (x*multiplier) + ',' + (end-start) + '\n';
+}
+
+fs.writeFile('timingTestResult.csv', csv);
